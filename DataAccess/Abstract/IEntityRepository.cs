caf742a3368b832cd2dc38efc5c67ee623980be8
Onur <onurozet@hotmail.com>
2022-09-02 23:22:30 +0300

    Add project files.

---
+++ DataAccess/Abstract/IEntityRepository.cs
@@ -0,0 +1,24 @@
+﻿using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Abstract
+{
+    public interface IEntityRepository<T> where T:class ,IEntity,new()
+    {
+
+        List<T> GetAll(Expression<Func<T,bool>> filter=null);  //Filtre vermesekte olur kodu
+        T Get(Expression<Func<T, bool>> filter);  // FIltre vermemizi saglar
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+
+       // List<T> GetAllByCategory(int categoryId);  Expression ile filtreleme yatik ve bu satirdaki koda ihtiyacimiz kalmadi
+
+
+    }
+}
---
+++ DataAccess/Concrete/InMemory/InMemoryProductDal.cs
@@ -0,0 +1,82 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete.InMemory
+{
+    public class InMemoryProductDal : IProductDal
+    {
+        List<Product> _products;
+
+        public InMemoryProductDal()
+        {
+
+            //sanki bir database den geliyormus gibi urun listesi olusturduk
+            _products = new List<Product> {
+              new Product{ProductId=1,CategoryId=1,ProductName="Bardak",UnitPrice=15,UnitsInStock=15},
+              new Product{ProductId=2,CategoryId=2,ProductName="Kamera",UnitPrice=500,UnitsInStock=3},
+              new Product{ProductId=3,CategoryId=2,ProductName="Telefon",UnitPrice=1500,UnitsInStock=15},
+              new Product{ProductId=4,CategoryId=2,ProductName="Klavye",UnitPrice=150,UnitsInStock=55},
+              new Product{ProductId=5,CategoryId=2,ProductName="Fare",UnitPrice=85,UnitsInStock=1}
+            };
+            
+        }
+        public void Add(Product product)
+        {
+            _products.Add(product);
+        }
+
+        public void Delete(Product product)
+        {
+
+            Product productToDelete=null;
+            //foreach (var p in _products)
+            //{
+            //    if (product.ProductId==p.ProductId)
+            //    {
+            //        productToDelete = p;
+            //    }
+            //}
+
+          productToDelete = _products.SingleOrDefault(p=>p.ProductId==product.ProductId); // ID aramalarinda genelde singleordefault kullanilir. sadece bir adet id bulur
+           _products.Remove(productToDelete);
+            
+        }
+
+        public Product Get(Expression<Func<Product, bool>> filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Product> GetAll()
+        {
+            return _products;
+        }
+
+        public List<Product> GetAll(Expression<Func<Product, bool>> filter = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Product> GetAllByCategory(int categoryId)
+        {
+          return  _products.Where(p=>p.CategoryId==categoryId).ToList();
+        }
+
+        public void Update(Product product)
+        {
+            //Gonderdigim urun id sine sahip olan listedeki  urunu bul
+            Product productToUpdated = _products.SingleOrDefault(p => p.ProductId == product.ProductId);
+            productToUpdated.ProductName = product.ProductName;
+            productToUpdated.CategoryId = product.CategoryId;
+            productToUpdated.UnitPrice = product.UnitPrice;
+            productToUpdated.UnitsInStock = product.UnitsInStock;
+
+        }
+    }
+}
